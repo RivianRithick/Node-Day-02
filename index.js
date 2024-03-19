d4d75d5b8873f36479bdbf9cebca647119439179
RivianRithick <rithickshival16@gmail.com>
2024-03-19 15:59:52 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import hallRouter from "./Routers/hallBooking.Router.js";
+
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200)
+    .send(`<div style="text-align: center; background-color:#FBF3D5;  padding: 10px;"><h1>Hall Booking API</h1></div>
+    <div>
+    <ul>
+    <li>
+    <h3><mark>Get:</mark> Use the endpoint to <mark>/hallapi/allroomdetails</mark> to get the All Room Details</h3>
+    </li>
+
+    <li>
+    <h3><mark>Post:</mark> Change the endpoint to <mark>/hallapi/createroom</mark> to Create the New Room</h3>
+    </li>
+
+    <li>
+    <h3><mark>Get:</mark> Change the endpoint to <mark>/hallapi/bookingroom</mark> to Book a New Room</h3>
+    </li>
+
+    <li>
+    <h3><mark>Get:</mark> Change the endpoint to <mark>/hallapi/bookedroomdata</mark> to retrieve Booked Room Data</h3>
+    </li>
+
+    <li>
+    <h3><mark>Get:</mark> Change the endpoint to <mark>/hallapi/customersbookeddata</mark> to retrieve Data of Booked Customers and Rooms</h3>
+    </li>
+
+    <li>
+    <h3><mark>Get:</mark> Change the endpoint to <mark>/hallapi/customerbookingcount</mark> to retrieve Booking Counts and Room Data for Booked Customers</h3>
+    </li>
+
+    </ul> 
+    </div>`);
+});
+app.use("/hallapi", hallRouter);
+
+app.listen(PORT, () => {
+  console.log(`App Listening in ${PORT}`);
+});
